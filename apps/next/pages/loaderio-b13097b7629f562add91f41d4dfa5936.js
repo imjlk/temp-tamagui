e24ee5fe58039bc476982eecff006d811e231dd3
imjlk <i@m.junglei.kim>
2022-11-28 15:27:48 +0900

    fix file for testing

--- apps/next/pages/loaderio-b13097b7629f562add91f41d4dfa5936.js
+++
@@ -1,3 +0,0 @@
-export default function loadTest() {
-	return 'loaderio-b13097b7629f562add91f41d4dfa5936';
-}
---
+++ apps/next/pages/loaderio-cd756cdc0d1539ffe0d0cff1decb28d8.js
@@ -0,0 +1,3 @@
+export default function loadTest() {
+	return 'loaderio-cd756cdc0d1539ffe0d0cff1decb28d8';
+}
